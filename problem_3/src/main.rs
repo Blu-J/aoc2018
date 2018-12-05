3be738ea517bdb2c78d1f2d1d9e3466ebb5a47e0
Justin Miller <mogulslayer@gmail.com>
2018-12-05 08:16:07 -0700

    feat: AOC Problem 3 Part 2

--- problem_3/src/main.rs
+++ problem_3/src/main.rs
@@ -3,7 +3,7 @@ extern crate im;
 extern crate lazy_static;
 extern crate regex;
 
-use im::{HashMap, HashSet};
+use im::hashset::HashSet;
 use regex::Regex;
 use std::cmp::max;
 use std::fs::File;
@@ -21,31 +21,36 @@ fn input() -> Result<String> {
 
 #[derive(Debug)]
 struct Claim {
+    id: u32,
     offset_x: u32,
     offset_y: u32,
     size_x: u32,
     size_y: u32,
 }
-fn part_1(input: &str) -> Result<u32> {
+fn claims_from_input(input: &str) -> Vec<Claim> {
     lazy_static! {
-        static ref regex: Regex = Regex::new(r"#\d+ @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+        static ref regex: Regex = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
     }
-
-    let claims = input
+    input
         .lines()
         .map(|line| {
             let capture = regex.captures(line).unwrap();
-            let offset_x = capture.get(1).unwrap().as_str().parse::<u32>().unwrap();
-            let offset_y = capture.get(2).unwrap().as_str().parse::<u32>().unwrap();
-            let size_x = capture.get(3).unwrap().as_str().parse::<u32>().unwrap();
-            let size_y = capture.get(4).unwrap().as_str().parse::<u32>().unwrap();
+            let id = capture.get(1).unwrap().as_str().parse::<u32>().unwrap();
+            let offset_x = capture.get(2).unwrap().as_str().parse::<u32>().unwrap();
+            let offset_y = capture.get(3).unwrap().as_str().parse::<u32>().unwrap();
+            let size_x = capture.get(4).unwrap().as_str().parse::<u32>().unwrap();
+            let size_y = capture.get(5).unwrap().as_str().parse::<u32>().unwrap();
             Claim {
+                id,
                 offset_x,
                 offset_y,
                 size_x,
                 size_y,
             }
-        }).collect::<Vec<Claim>>();
+        }).collect()
+}
+fn part_1(input: &str) -> Result<u32> {
+    let claims = claims_from_input(input);
 
     let (max_x, max_y) = claims.iter().fold((0u32, 0u32), |acc, claim| {
         (
@@ -84,10 +89,54 @@ fn test_part_1() {
     assert_eq!(4, part_1(TEST_INPUT_1).unwrap());
 }
 
+fn part_2(input: &str) -> Result<u32> {
+    let claims = claims_from_input(input);
+    let (max_x, max_y) = claims.iter().fold((0u32, 0u32), |acc, claim| {
+        (
+            max(acc.0, claim.offset_x + claim.size_x),
+            max(acc.1, claim.offset_y + claim.size_y),
+        )
+    });
+
+    let mut grid: Vec<Vec<Option<u32>>> =
+        vec![vec![None; (max_y + 2) as usize]; (max_x + 2) as usize];
+    let mut found_ids: HashSet<u32> = HashSet::new();
+
+    for claim in claims {
+        found_ids.insert(claim.id);
+        for x in claim.offset_x..(claim.offset_x + claim.size_x) {
+            for y in claim.offset_y..(claim.offset_y + claim.size_y) {
+                match grid[x as usize][y as usize] {
+                    Some(other_id) => {
+                        found_ids.remove(&other_id);
+                        found_ids.remove(&claim.id);
+                    }
+                    None => {
+                        grid[x as usize][y as usize] = Some(claim.id);
+                    }
+                }
+            }
+        }
+    }
+
+    found_ids
+        .iter()
+        .cloned()
+        .nth(0)
+        .ok_or_else(|| "There are no elements at 0".into())
+}
+
+#[test]
+fn test_part_2() {
+    assert_eq!(3, part_2(TEST_INPUT_1).unwrap());
+}
+
 fn main() -> Result<()> {
     let input_read = input()?;
     let answer_part_1 = part_1(&input_read)?;
+    let answer_part_2 = part_2(&input_read)?;
 
     println!("Answer for part 1 is {}", answer_part_1);
+    println!("Answer for part 2 is {}", answer_part_2);
     Ok(())
 }
