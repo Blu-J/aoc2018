69e9e6ba1b390313ed44f9c69642faaac00a0b34
Justin Miller <mogulslayer@gmail.com>
2018-12-07 08:40:48 -0700

    feat: day 5 part 2

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -15,6 +15,14 @@ struct Part1 {
     value: Vec<char>,
 }
 
+fn opposite(value: char) -> char {
+    if value.is_uppercase() {
+        value.to_lowercase().nth(0).unwrap()
+    } else {
+        value.to_uppercase().nth(0).unwrap()
+    }
+}
+
 impl Part1 {
     fn add_char(self, new_value: char) -> Part1 {
         match self.value.as_slice().split_first() {
@@ -22,12 +30,7 @@ impl Part1 {
                 value: vec![new_value],
             },
             Some((&head, tail)) => {
-                let opposite: char = if new_value.is_uppercase() {
-                    new_value.to_lowercase().nth(0).unwrap()
-                } else {
-                    new_value.to_uppercase().nth(0).unwrap()
-                };
-                if opposite == head {
+                if opposite(new_value) == head {
                     Part1 { value: tail.into() }
                 } else {
                     let mut value = vec![new_value, head];
@@ -56,10 +59,33 @@ fn test_part_1() {
     assert_eq!(6, part_1("aabAAB").unwrap());
 }
 
+fn part_2(input: &str) -> Result<u32> {
+    let mut answer = Part1 { value: vec![] };
+    for c in input.chars() {
+        answer = answer.add_char(c);
+    }
+    for remove_char in "abcdefghijklmnopqrstuvwxyz".chars() {
+        let opposite_remove_char = opposite(remove_char);
+        let mut local_answer = Part1 { value: vec![] };
+        for c in input.chars() {
+            if !(c == remove_char || c == opposite_remove_char) {
+                local_answer = local_answer.add_char(c);
+            }
+        }
+        if answer.value.len() > local_answer.value.len() {
+            answer = local_answer;
+        }
+    }
+    Ok(answer.value.len() as u32)
+}
+
 fn main() -> Result<()> {
     let input_read = input()?;
     let answer_part_1 = part_1(&input_read)?;
 
     println!("Answer for part 1 is {}", answer_part_1);
+    let answer_part_2 = part_2(&input_read)?;
+
+    println!("Answer for part 2 is {}", answer_part_2);
     Ok(())
 }
