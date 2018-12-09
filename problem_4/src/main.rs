d4a8b6754be328371abb76ef66d2c1cfcd2e15b6
Justin Miller <mogulslayer@gmail.com>
2018-12-09 13:51:19 -0700

    feat: Day 4 part 2

--- problem_4/src/main.rs
+++ problem_4/src/main.rs
@@ -1,9 +1,7 @@
-// // extern crate im;
 #[macro_use]
 extern crate lazy_static;
-// extern crate regex;
 
-use im::{HashMap, HashSet};
+use im::HashMap;
 use regex::Regex;
 use std::error::Error;
 use std::fs::File;
@@ -19,77 +17,108 @@ fn input() -> Result<String> {
     buf_reader.read_to_string(&mut contents)?;
     Ok(contents)
 }
-type Sleeping_Amount = HashMap<u32, u32>;
+type SleepingAmount = HashMap<u32, u32>;
 
-fn part_1(input: &str) -> Result<u32> {
-    let sorted_events: Vec<Event> = {
-        let mut events = input
-            .lines()
-            .map(|x| x.parse::<Event>())
-            .filter_map(Result::ok)
-            .collect::<Vec<Event>>();
-        events.sort();
-        events
-    };
-    // println!("So far the sorted_events are {:?}", sorted_events);
+fn sort_events(input: &str) -> Vec<Event> {
+    let mut events = input
+        .lines()
+        .map(|x| x.parse::<Event>())
+        .filter_map(Result::ok)
+        .collect::<Vec<Event>>();
+    events.sort();
+    events
+}
 
-    // Hash all the total times together
-    let id_sleep = {
-        let mut id_2_sleep: HashMap<Id, Sleeping_Amount> = HashMap::new();
-        let mut current_id: Option<Id> = None;
-        let mut start_time: Option<Time> = None;
-        for event in sorted_events {
-            match event.kind {
-                Kind::StartShift { id } => {
-                    current_id = Some(id);
-                    start_time = None;
-                }
-                Kind::Sleep => {
-                    start_time = Some(event.time);
-                }
-                Kind::Wake => {
-                    let mut value = id_2_sleep
-                        .entry(current_id.unwrap())
-                        .or_insert_with(|| HashMap::new());
-                    if let Some(start_time) = start_time {
-                        for i in (start_time.minute)..event.time.minute {
-                            let mut time_value = value.entry(i).or_insert(0);
-                            *time_value += 1;
-                        }
+type IdSleep = HashMap<Id, SleepingAmount>;
+fn hash_event_times(events: &[Event]) -> IdSleep {
+    let mut id_sleep: IdSleep = HashMap::new();
+    let mut current_id: Option<Id> = None;
+    let mut start_time: Option<Time> = None;
+    for event in events {
+        match event.kind {
+            Kind::StartShift { id } => {
+                current_id = Some(id);
+                start_time = None;
+            }
+            Kind::Sleep => {
+                let cloned_time = Time {
+                    year: event.time.year,
+                    month: event.time.month,
+                    day: event.time.day,
+                    hour: event.time.hour,
+                    minute: event.time.minute,
+                };
+                start_time = Some(cloned_time);
+            }
+            Kind::Wake => {
+                let value = id_sleep
+                    .entry(current_id.unwrap())
+                    .or_insert_with(HashMap::new);
+                if let Some(start_time) = start_time {
+                    for i in (start_time.minute)..event.time.minute {
+                        let time_value = value.entry(i).or_insert(0);
+                        *time_value += 1;
                     }
-                    start_time = None;
                 }
+                start_time = None;
             }
         }
-        id_2_sleep
-    };
-
-    // println!("So far the current id_2_sleep is {:?}", id_2_sleep);
-    // Find the biggest time
-    {
-        let mut id_2_sleep: Vec<(Id, Sleeping_Amount)> = id_sleep.iter().cloned().collect();
-        id_2_sleep.sort_by_key(|(_, sleeping_amount)| sleeping_amount.values().sum::<u32>());
-        println!("Items are {:?}", id_2_sleep);
-        id_2_sleep
-            .last()
-            .ok_or_else(|| "Could not find the first item".into())
-            .map(|(left, sleeping_amount)| {
-                let mut sleeping_amount: Vec<(u32, u32)> =
-                    sleeping_amount.iter().cloned().collect();
-                sleeping_amount.sort_by_key(|(_, amount)| *amount);
-                let found_time = sleeping_amount.last().unwrap();
-                println!("Found the sleeping {:?}", sleeping_amount);
-                println!("Found the time {:?}", found_time);
-                (*left) * (found_time.0)
-            })
     }
+    id_sleep
+}
+
+fn part_1(id_sleep: &IdSleep) -> Result<u32> {
+    // Find the biggest time
+    let mut id_2_sleep: Vec<(Id, SleepingAmount, u32)> = id_sleep
+        .iter()
+        .cloned()
+        .map(|(key, sleeping_time)| {
+            let total_time = sleeping_time.values().cloned().sum::<u32>();
+            (key, sleeping_time, total_time)
+        })
+        .collect();
+    id_2_sleep.sort_by_key(|&(_, _, total_time)| total_time);
+    id_2_sleep
+        .last()
+        .ok_or_else(|| "Could not find the first item".into())
+        .map(|(left, sleeping_amount, _)| {
+            let mut sleeping_amount: Vec<(u32, u32)> = sleeping_amount.iter().cloned().collect();
+            sleeping_amount.sort_by_key(|(_, amount)| *amount);
+            let found_time = sleeping_amount.last().unwrap();
+            (*left) * (found_time.0)
+        })
+}
+fn part_2(id_sleep: &IdSleep) -> Result<u32> {
+    let with_highest_sleep = id_sleep.iter().cloned().map(|(key, sleeping_time)| {
+        let total_time = sleeping_time
+            .iter()
+            .cloned()
+            .max_by_key(|(_, sleep_count)| *sleep_count)
+            .unwrap();
+        (key, sleeping_time, total_time)
+    });
+
+    let last_item: (Id, (u32, u32)) =
+        with_highest_sleep.fold((0, (0, 0)), |acc, (id, _, (max_hour, max_hour_count))| {
+            let (_id, (_max_hour, acc_max_hour_count)) = acc;
+            if max_hour_count > acc_max_hour_count {
+                return (id, (max_hour, max_hour_count));
+            }
+            acc
+        });
+    let (id, (max_hour, _)) = last_item;
+    Ok(id * max_hour)
 }
 
 fn main() -> Result<()> {
     let input_read = input()?;
-    let answer_part_1 = part_1(&input_read)?;
+    let events = sort_events(&input_read);
+    let id_sleep = hash_event_times(&events);
+    let answer_part_2 = part_2(&id_sleep)?;
+    let answer_part_1 = part_1(&id_sleep)?;
 
     println!("Answer for part 1 is {:?}", answer_part_1);
+    println!("Answer for part 2 is {:?}", answer_part_2);
     Ok(())
 }
 
@@ -162,7 +191,9 @@ impl FromStr for Event {
     }
 }
 
-const INPUT_1: &str = r#"[1518-11-01 00:00] Guard #10 begins shift
+#[test]
+fn test_case() {
+    const INPUT_1: &str = r#"[1518-11-01 00:00] Guard #10 begins shift
 [1518-11-01 00:05] falls asleep
 [1518-11-01 00:25] wakes up
 [1518-11-01 00:30] falls asleep
@@ -179,9 +210,10 @@ const INPUT_1: &str = r#"[1518-11-01 00:00] Guard #10 begins shift
 [1518-11-05 00:03] Guard #99 begins shift
 [1518-11-05 00:45] falls asleep
 [1518-11-05 00:55] wakes up"#;
-#[test]
-fn test_1() {
-    assert_eq!(240, part_1(INPUT_1).unwrap());
+    let events = sort_events(&INPUT_1);
+    let id_sleep = hash_event_times(&events);
+    assert_eq!(240, part_1(&id_sleep).unwrap());
+    assert_eq!(4455, part_2(&id_sleep).unwrap());
 }
 #[test]
 fn test_parse_line() {
